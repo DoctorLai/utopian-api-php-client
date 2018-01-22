df42411838af654cc752e9411e2c0eb5570126e2
@justyy <oh@steakovercooked.com>
2018-01-22 18:43:17 +0000

    Create stats_test.php

---
+++ tests/stats_test.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../class.stats.php';
+
+use PHPUnit\Framework\TestCase;
+
+class UtopianTests_stats_tests extends TestCase {
+  
+  public function test_getcategories_not_null() {
+     $stats = new Stats();
+     $this->assertTrue($stats->GetCategories() != null);
+  }  
+  
+  public function test_getcategory_blog_not_null() {
+     $stats = new Stats();
+     $this->assertTrue($stats->GetCategory() != null);
+  }
+  
+  public function test_getvalue_total_paid_authors() {
+     $stats = new Stats();
+     $this->assertTrue($stats->GetValue('total_paid_authors') > 0);
+  }  
+}
